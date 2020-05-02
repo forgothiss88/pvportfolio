955155da4e560d2e3cbea00bacedca4091871a6d
andrea_desktop_ubuntu <andrea.vitov@gmail.com>
2020-05-02 20:27:18 +0200

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+function adjustTopbarHeight() {
+  h = $("#topbar").outerHeight() + 6;
+  $("#topbarborder").parent().height(h);
+  $("#mySidepanel").css("top", h-6);
+}
+
+function toggleHamburger(ham) {
+  $(ham).toggleClass("is-active")
+  toggleSidePanel()
+  togglePVBorder()
+  toggleTopBarBorder()
+}
+
+function toggleSidePanel() {
+  $("#mySidepanel").toggleClass("open")
+}
+
+function togglePVBorder() {
+  $("#pv").toggleClass("active")
+  $("#pv > i").toggleClass("active")
+  $("#pv > h1").toggleClass("active")
+
+}
+function toggleTopBarBorder() {
+  $(".topbarborder").toggleClass("closed")
+}
+
+function toggleImg(arr) {
+  $(arr).parent().next("img").toggleClass("w3-hide")
+}
