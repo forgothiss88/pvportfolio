cfeb8ff719293515ef7200290087b604787b8b2b
Andrea Vitali <andrea.vitali@etiqa.it>
2022-03-23 18:13:42 +0100

    fixed webpack dependancies and usage

--- webpack.config.js
+++ webpack.config.js
@@ -8,16 +8,13 @@ module.exports = {
     path: "/Users/andreavitali/Desktop/Projects/pvportfolio/assets/js/",
     filename: "bundle.js",
   },
-  // module: {
-  //   loaders: [
-  //     {
-  //       test: /\.jsx?$/,
-  //       exclude: /(node_modules)/,
-  //       loader: "babel", // "babel-loader" is also a legal name to reference
-  //       query: {
-  //         presets: ["react", "es2015"],
-  //       },
-  //     },
-  //   ],
-  // },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
 };
--- webpack/entry.js
+++ webpack/entry.js
@@ -1,1 +1,4 @@
-console.log("Hello, world!")
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(React.createElement(), document.getElementById("root"));
